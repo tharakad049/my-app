bce160677fa89f6f4322dfef8befacfe06539e03
DilanTharaka <tharakad049@gmail.com>
2022-07-14 21:11:28 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import App from '../../my-app/src/app/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,8 +10,8 @@ root.render(
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    
   </React.StrictMode>
+ty
 );
 
 // If you want to start measuring performance in your app, pass a function
