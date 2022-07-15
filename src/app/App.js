451c287fbf395ef3dd9308aaa5ec0f3710abc397
DilanTharaka <tharakad049@gmail.com>
2022-07-15 10:57:35 +0530

    update layout

--- src/app/App.js
+++ src/app/App.js
@@ -2,15 +2,21 @@ import React from "react";
 import HomePage from "../pages/Home";
 import Login from "../pages/Login";
 import {Route, Routes} from "react-router-dom"
+import NotFound from "../pages/session/NotFound";
+import FlexBoxLayout from "../pages/Layouts/FlexBox";
+import GridLayout from "../pages/Layouts/Gird";
+import Posts from "../../src/pages/Posts";
 
 function App() {
   return (
-    
-    //<Login/>//
-
-<Routes>
-  <Route exact path = "/" element={<HomePage/>}></Route>
-</Routes>
+    <Routes>
+        <Route exact path='/' element={<HomePage/>}/>
+        <Route exact path='posts' element={<Posts/>}/>
+        <Route path='login' element={<Login/>}/>
+        <Route path='flex-layout' element={<FlexBoxLayout/>}/>
+        <Route path='grid-layout' element={<GridLayout/>}/>
+        <Route path="*" element={<NotFound/>}/>
+    </Routes>
 
   );
 }
